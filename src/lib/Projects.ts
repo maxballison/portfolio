fb0681ef7cd9650212ed3185b1d840242b87c682
MAX ALLISON <MALLISON@COLLEGE.HARVARD.EDU>
2023-11-21 20:27:09 -0500

    fixed verbiage

--- src/lib/Projects.ts
+++ src/lib/Projects.ts
@@ -3,14 +3,14 @@ const projects = [
 		title: 'Dao',
 		technologies: ['python', 'p5js', 'NRCLex', 'Tonejs'],
 		description:
-			'Dao is a data and music visualization of popular books/films and old philosophical Chinese texts. It uses a python sentiment analysis algorithm analyze the emotions present in a text and live draw pictures/create music based on the emotions in each section of the text',
+			'Dao is a data and music visualization of popular books/films and old philosophical Chinese texts. It uses a python sentiment analysis algorithm to analyze the emotions present in a text and live draw pictures/create music based on the emotions in each section of the text',
 		url: 'https://maxballison.github.io/Dao/'
 	},
 	{
 		title: 'Notes on Love',
 		technologies: ['python', 'p5js', 'gpt4'],
 		description:
-			"Notes on Love is an application meant to categorize Harvard/MIT students' perspectives on Love. It uses a database of hundreds of love letters that a small team collected. I run each letter through an algorithm that uses gpt4 give the letter values for originality and positivity, and place the letter on a graph",
+			"Notes on Love is an application meant to categorize Harvard/MIT students' perspectives on Love. It uses a database of hundreds of love letters collected by a small team. I run each letter through an algorithm that uses gpt4 to give the letter values for originality and positivity, and place the letter on a graph",
 		url: 'https://maxballison.github.io/love-notes/'
 	},
 	{
